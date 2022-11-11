34e16d39b4a66c364406af838a0b707a1b3e732a
aamHimel <aam.himel@gmail.com>
2022-11-11 18:19:58 +0600

    final commit before review

--- js/app.js
+++ js/app.js
@@ -12,3 +12,11 @@ function readingTime() {
   }
 }
 readingTime();
+
+(function () {
+  var toggle = document.querySelector(".nav-toggle");
+
+  toggle.addEventListener("click", function (e) {
+    this.classList.toggle("opened");
+  });
+})();
