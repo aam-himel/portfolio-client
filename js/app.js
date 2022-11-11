58d697412e342fd0328437fc7bf72f67864b5168
aamHimel <aam.himel@gmail.com>
2022-11-11 09:42:55 +0600

    projects edit

--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
-const nav = document.getElementById("nav");
-const links = document.querySelectorAll("nav li");
+function readingTime() {
+  const projects = document.getElementById("projects");
+  const articles = projects.querySelectorAll(".article__content");
+  const readings = projects.querySelectorAll(".article__reading");
+
+  for (let i = 0; i < articles.length; i++) {
+    const text = articles[i].innerText;
+    const wpm = 225;
+    const words = text.trim().split(/\s+/).length;
+    const time = Math.ceil(words / wpm);
+    readings[i].getElementsByTagName("span")[0].innerText = time;
+  }
+}
+readingTime();
